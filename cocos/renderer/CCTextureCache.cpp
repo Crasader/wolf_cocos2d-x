39bbe38d91330317ab6b4fbc6786dd1c41b3e7ed
lyd <zl@LyddeMac-mini.local>
2018-02-24 11:06:17 +0800

    test

--- cocos/renderer/CCTextureCache.cpp
+++ cocos/renderer/CCTextureCache.cpp
@@ -4,6 +4,7 @@ Copyright (c) 2010-2012 cocos2d-x.org
 Copyright (c) 2011      Zynga Inc.
 Copyright (c) 2013-2017 Chukong Technologies Inc.
 
+ 
 http://www.cocos2d-x.org
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
